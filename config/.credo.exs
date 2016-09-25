6bbecb9b5f746ea08e366a9627eb35926dedd7d3
Mahendra Kalkura <mahendra@kalkura.com>
2016-09-25 18:50:46 +0530

    config/.credo.exs

--- config/.credo.exs
+++ config/.credo.exs
@@ -1,17 +1,8 @@
 %{
   configs: [
     %{
-      checks: [
-        {Credo.Check.Design.AliasUsage, false},
-      ],
-      files: %{
-        included: [
-          "config/",
-          "lib/",
-          "test/",
-          "*.exs",
-        ],
-      },
+      checks: [{Credo.Check.Design.AliasUsage, false}],
+      files: %{included: ["config/", "lib/", "test/", "*.exs"]},
       name: "default",
     },
   ],
