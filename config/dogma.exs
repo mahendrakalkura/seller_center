35d2a8b8f5b00e2bea7e9705c73714680bf077da
Mahendra Kalkura <mahendra@kalkura.com>
2016-09-25 18:50:51 +0530

    config/dogma.exs

--- config/dogma.exs
+++ config/dogma.exs
@@ -4,9 +4,5 @@ require Dogma.RuleSet
 use Mix.Config
 
 config :dogma,
-  override: [
-    %Dogma.Rule.FunctionParentheses{
-      enabled: false,
-    },
-  ],
+  override: [%Dogma.Rule.FunctionParentheses{enabled: false}],
   rule_set: Dogma.RuleSet.All
